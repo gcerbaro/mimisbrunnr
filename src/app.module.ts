db923e357785766b73479247909a8cba11207a08
gcerbaro <155097@upf.br>
2026-07-27 11:51:05 -0300

    feature: add DB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: 'database.sqlite3',
+      autoLoadEntities: true,
+      synchronize: true, //Only for dev, switch to TypeORM migrations when ready for prod
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
