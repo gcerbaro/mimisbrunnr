205fc5f8a18434f337041e01e797c8c348d53c8e
gcerbaro <155097@upf.br>
2026-07-28 17:59:12 -0300

    feature: add password hashing

--- src/modules/user/model/user.entity.ts
+++ src/modules/user/model/user.entity.ts
@@ -1,31 +1,47 @@
-import { hash } from 'crypto';
-//import { Column, Entity, Index, JoinColumn, OneToOne } from 'typeorm';
+import { Column, Entity, Index, JoinColumn, OneToOne } from 'typeorm';
+import {Exclude} from 'class-transformer';
+//import {SaltRounds} from 'env';
+import  {verify, hash} from "argon2";
 
 export enum Role {
     USER = "user",
     ADMIN = "admin"
 }
 
+@Entity()
 export class User {
+    @Column({type: 'varchar', length: 100})
     name: string;
+
+    @Column({ type: 'varchar', length: 255, unique: true })
     email: string;
+
+    @Column({ type: 'varchar', length: 60, select: false })
     password: string;
+
+    @Column({type: 'timestamp'})
     birthday: Date;
+
+    @Column({type: 'varchar', length: 255})
     profilePicture: string;
+
     session: string;
+
+    @Column({
+        type: 'enum',
+        enum: Role,
+        default: Role.USER
+    })
     role: Role;
+
+    //@Column({type: 'varchar', nullable: true})
     //resetPasswordToken: string | null;
+    //@Column({type: 'timestamp', nullable: true})
     //resetPasswordExpires: Date | null;
+    //@Column({type: 'varchar', nullable: true})
     //totpSecret: string;
+    //@Column({type: 'boolean', default: false})
     //totpEnabled: boolean;
-
-
-    static async comparePassword(
-        password: string | Buffer,
-        hash: string,
-    ): Promise<boolean> {
-        return compare(password, hash);
-    } 
 
     async comparePassword(pwd: string | Buffer): Promise<Boolean> {
         let hash = this.password;
@@ -40,9 +56,15 @@ export class User {
         return User.comparePassword(pwd, hash);
     }
 
+    static async comparePassword(
+        password: string | Buffer,
+        hash: string,
+    ): Promise<boolean> {
+        return verify(hash, password);
+    } 
+
     static async hashPassword(pwd: string | Buffer): Promise<string> {
-        return hash(pwd, argon);
-        //change for whatever method is called by argon2
+        return await hash(pwd);
     }
 
 
