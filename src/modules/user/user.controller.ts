29bba230f6d7ab248911f332bec1a4102496d679
gcerbaro <155097@upf.br>
2026-08-12 23:02:05 -0300

    feature: add user preference routes

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, ParseUUIDPipe, Patch, Post, Query } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, Param, ParseUUIDPipe, Patch, Post, Put, Query } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { UserCreateDTO } from "./dtos/create-user.dto";
 import { UserDTO } from "./dtos/user.dto";
@@ -7,6 +7,7 @@ import { Role, User } from "./model/user.entity";
 import { UserInternalUpdateDto } from "./dtos/user-internal-updates.dto";
 import { Authorize, LoggedUser, Public } from "../auth/auth.decorator";
 import { UserQueryDTO } from "./dtos/user-query.dto";
+import { UpdateUserPreferencesDTO } from "./dtos/update-userpreferences.dto";
 
 @Controller('users')
 export class UserController {
@@ -52,6 +53,21 @@ export class UserController {
         @Body() updateDto: UserInternalUpdateDto,
     ) {
         return this.userService.updateByAdmin(admin, userId, updateDto);
+    }
+
+    @Put()
+    async updateUserPreferences(
+        @LoggedUser() user,
+        @Body() updateDto: UpdateUserPreferencesDTO,
+    ){
+        return this.updateUserPreferences(user, updateDto);
+    }
+
+    @Put()
+    async resetUserPreferences(
+        @LoggedUser() user,
+    ){
+        return this.resetUserPreferences(user);
     }
 /* 
     @Delete(':id')
